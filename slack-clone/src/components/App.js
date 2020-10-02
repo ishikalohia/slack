409778719c694d380d1fc1acaedad490cf371282
Ishika <ishikalohia4@gmail.com>
2020-10-02 12:25:11 +0530

    Add Route

--- slack-clone/src/components/App.js
+++ slack-clone/src/components/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import SignIn from './SignIn';
 
+function Home() {
+  return <div>home</div>;
+}
 
-function App() {
-  return (
-    <div className="App">
-        HEllo World
-        <SignIn />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/home" component={Home} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- slack-clone/src/components/SignIn.js
+++ slack-clone/src/components/SignIn.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 
 export default class SignIn extends Component {
   render() {
-    return <div>SignIn</div>;
+    return (
+      <div className="signup-form">
+        <form>
+          <h1>Sign In/ Sign Up</h1>
+          <button>Sign In with Google/Sign Up with Google</button>
+        </form>
+      </div>
+    );
   }
 }
