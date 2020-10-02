d7b1a53bc724b9ef7f9f48f3a2ed121263dfc9ae
Ishika <ishikalohia4@gmail.com>
2020-10-02 13:30:29 +0530

    Update Sidebar

--- slack-clone/src/components/Sidebar.js
+++ slack-clone/src/components/Sidebar.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 
 export default class Sidebar extends Component {
   render() {
-    return <div>Sidebar</div>;
+    return (
+      <div id="sidebar">
+        <div className="user-profile">
+          <div>
+            <img />
+          </div>
+          <div>Ronald</div>
+        </div>
+        <div className="user-channels"></div>
+      </div>
+    );
   }
 }
